98632218ffc9928e61949aa473dce672f8612bc5
JesselynHuang <91172633+JesselynHuang@users.noreply.github.com>
2022-03-05 00:31:43 +0700

    [JH] - Integrate Notification Remote Android

--- Screens/Notification.js
+++ Screens/Notification.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import notifee, {EventType} from '@notifee/react-native';
 import NotificationService from '../libs/NotificationService';
+import messaging from '@react-native-firebase/messaging';
 
 function Notification(props) {
   useEffect(() => {
@@ -24,33 +25,70 @@ function Notification(props) {
 
   //notifeePermission
   useEffect(() => {
-    requestPermission();
+    // requestPermission();
+    requestUserPermission();
   });
 
-  //   function _onShowNotification() {
-  //     NotificationService.onDisplayNotification('Notif Title', 'Notif Des');
-  //   }
+  function _onShowNotification() {
+    NotificationService.onDisplayNotification('Notif Title', 'Notif Des');
+  }
 
-  //   messaging().onMessage(async remoteMessage => {
-  //     NotificationService.onDisplayNotification(
-  //       remoteMessage.notification.title,
-  //       remoteMessage.notification.body,
-  //       remoteMessage.data,
-  //     );
-  //   });
+  messaging().onMessage(async remoteMessage => {
+    NotificationService.onDisplayNotification(
+      remoteMessage.notification.title,
+      remoteMessage.notification.body,
+      remoteMessage.data,
+    );
+  });
+
+  // const onDisplayRemoteNotification = async () => {
+  //   const authStatus = await messaging().requestPermission();
+  //   const enabled =
+  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  //   if (enabled) {
+  //     await messaging()
+  //       .getToken()
+  //       .then(fcmToken => {
+  //         console.log('fcmToken -> ', fcmToken);
+  //       });
+  //   }
+  // };
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      console.log('Authorization Status : ', authStatus);
+      getFcmToken();
+    }
+  }
+
+  const getFcmToken = async () => {
+    try {
+      const fcmToken = await messaging().getToken();
+      console.log('fcme token generated', fcmToken);
+    } catch (error) {
+      console.log('error', error);
+      // alert(error?.message);
+    }
+  };
 
   const requestPermission = async () => {
-    const authService = await messaging().requestPermission();
+    const authStatus = await messaging().requestPermission();
   };
 
   return (
     <View>
       <Text>This Is Notification Screen</Text>
-      <TouchableOpacity
-
-      //onPress={() => _onShowNotification()}
-      >
+      <TouchableOpacity onPress={() => _onShowNotification()}>
         <Text>{' Notif'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => onDisplayRemoteNotification()}>
+        <Text>{' Get Token'}</Text>
       </TouchableOpacity>
     </View>
   );
