e18d6780d999299fe3c340b414997257a8d4ce1b
JesselynHuang <91172633+JesselynHuang@users.noreply.github.com>
2022-03-08 19:02:49 +0700

    [JH] - 60% Integrate SignIn Google Android

--- Screens/Task2/Google.js
+++ Screens/Task2/Google.js
@@ -1,6 +1,49 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {
+  GoogleSignin,
+  statusCodes,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 
 export default function Google() {
-  return <Text>Google</Text>;
+  GoogleSignin.configure({
+    webClientId:
+      '785278866096-191tk3mavcdh07g3jtb3trmeepop3v2c.apps.googleusercontent.com',
+  });
+
+  async function signIn() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('userInfo--->'.userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('error1--->', error);
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('error2--->', error);
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('error3--->', error);
+        // play services not available or outdated
+      } else {
+        console.log('error4--->', error);
+        // some other error happened
+      }
+    }
+  }
+
+  return (
+    <View>
+      <Text>Google</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+        // disabled={this.state.isSigninInProgress}
+      />
+    </View>
+  );
 }
