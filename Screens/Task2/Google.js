68f9fe9663be72d2690756ab06e7de0a2f133442
JesselynHuang <91172633+JesselynHuang@users.noreply.github.com>
2022-03-09 11:08:37 +0700

    [JH] - Integrate Android & IOS Google Sign In

--- Screens/Task2/Google.js
+++ Screens/Task2/Google.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {
   GoogleSignin,
   statusCodes,
   GoogleSigninButton,
 } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
 export default function Google() {
-  GoogleSignin.configure({
-    webClientId:
-      '785278866096-191tk3mavcdh07g3jtb3trmeepop3v2c.apps.googleusercontent.com',
-  });
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+  const [user, setUser] = useState();
+  const [initializing, setInitializing] = useState(true);
 
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '785278866096-t6b0qbgcea3e69i86nan0cohrqbsoqg9.apps.googleusercontent.com',
+    });
+  }, []);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  console.log('userinfo-------->', userInfo);
   async function signIn() {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
-      console.log('userInfo--->'.userInfo);
+      setuserInfo(userInfo);
+
+      const {accessToken, idToken} = await GoogleSignin.signIn();
+      // setloggedIn(true);
+      const credential = auth.GoogleAuthProvider.credential(
+        idToken,
+        accessToken,
+      );
+      let loginUser = await auth().signInWithCredential(credential);
+      setloggedIn(true);
+      console.log('loginUser', loginUser);
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         console.log('error1--->', error);
